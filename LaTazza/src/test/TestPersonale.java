96079965c76d6d2a043b98e57b27ee8fec348310
nico <nico@guainazzo.it>
2021-03-19 11:56:24 +0100

    TESTPERSONALE

--- LaTazza/src/test/TestPersonale.java
+++ LaTazza/src/test/TestPersonale.java
@@ -1,162 +1,88 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 
 import application.model.utenti.Persona;
 import application.model.utenti.Personale;
 import application.utils.Euro;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class TestPersonale {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(OrderAnnotation.class)
+class OrderedTestsDemo {
 
 	Personale personale;
-	
+	int i;
+
 	@BeforeEach
 	void setUp() throws Exception {
 		personale = new Personale();
-		personale.addPersona("persona1");
-		personale.addPersona("persona2");
-		personale.addPersona("persona3");
+		personale.load();
 	}
-	
-	
+
 	@Test
+	@Order(1)
 	void testGetPersonale() {
-		assertEquals(personale.getPersonale().size(),3);
+		i = personale.getPersonale().size();
+		assertEquals(personale.getPersonale().size(), i);
+
 	}
-	
+
 	@Test
+	@Order(2)
+	void testAddPersonale() {
+		i = personale.getPersonale().size();
+		Persona p = new Persona("persona1");
+		personale.addPersona(p.getNome());
+		assertEquals(personale.getPersonale().size(), i + 1);
+		personale.removePersona(p);
+
+	}
+
+	@Test
+	@Order(5)
 	void testGetIndebitatiSize() {
-		for(Persona persona : personale.getPersonale()) {
-			persona.aumentaDebito(new Euro(5));
+		i = personale.getIndebitati().size();
+		Persona p = new Persona("persona1");
+		personale.addPersona(p.getNome());
+		for (Persona persona : personale.getPersonale()) {
+			if (persona.getNome() == p.getNome()) {
+				p.aumentaDebito(new Euro(5));
+			}
 		}
-		assertEquals(personale.getIndebitati().size(),3);
+		assertEquals(personale.getIndebitati().size(), i+1);
+		personale.diminuisciDebito(p, new Euro(5));
+		personale.removePersona(p);
 	}
-	
+
 	@Test
-	void testGetIndebitatiDebito() {
-		for(Persona persona : personale.getPersonale()) {
-			persona.aumentaDebito(new Euro(5));
-		}
-		for(Persona persona : personale.getIndebitati()) {
-			assertEquals(persona.getDebito(),new Euro(5));
-		}
-	}
-	
-	@AfterAll
-	public void cleanUp() throws Exception {
-		for(Persona persona : personale.getPersonale()) {
-			personale.removePersona(persona);
-		}
-	}
-	
-	/*
-	@Test
-	void testAddPersonaTrue() {
-		assertTrue(personale.addPersona("persona4"));
-	}
-	
-	@Test
-	void testAddPersonaFalse() {
-		assertFalse(personale.addPersona("persona1"));
-	}
-	
-	@Test
-	void testRemovePersonaTrue() {
-		assertTrue(personale.removePersona(new Persona("persona1")));
-	}
-	
-	@Test
-	void testRemovePersonaFalse() {
-		Persona pers=null;
-		for(Persona persona : personale.getPersonale()) {
-			pers=persona;
-			persona.aumentaDebito(new Euro(5));
-		}
-		assertFalse(personale.removePersona(pers));
-	}
-	
-	@Test
+	@Order(4)
 	void testDiminuisciDebitoTrue() {
-		for(Persona persona : personale.getPersonale()) {
-			persona.aumentaDebito(new Euro(5));
-			assertTrue(personale.diminuisciDebito(persona, new Euro(5)));
+		i = personale.getIndebitati().size();
+		Persona p = new Persona("persona1");
+		personale.addPersona(p.getNome());
+		for (Persona persona : personale.getPersonale()) {
+			if (persona.getNome() == p.getNome()) {
+				personale.diminuisciDebito(p, new Euro(0));
+			}
 		}
-		assertEquals(personale.getPagamentiDebito().size(),3);
+		assertEquals(p.getDebito(), new Euro(0));
+		personale.removePersona(p);
 	}
-	
+
 	@Test
-	void testDiminuisciDebitoFalseAmmontareMaggioreDebito() {
-		for(Persona persona : personale.getPersonale()) {
-			persona.aumentaDebito(new Euro(5));
-			assertFalse(personale.diminuisciDebito(persona, new Euro(8)));
-		}
+	@Order(3)
+	void testRemovePersonale() {
+		i = personale.getPersonale().size();
+		Persona p = new Persona("persona1");
+		personale.addPersona(p.getNome());
+		assertEquals(personale.getPersonale().size(), i+1);
+		personale.removePersona(p);
 	}
-	
-	@Test
-	void testDiminuisciDebito() {
-		for(Persona persona : personale.getPersonale()) {
-			persona.aumentaDebito(new Euro(5));
-			personale.diminuisciDebito(persona, new Euro(3));
-		}
-		for(Persona persona : personale.getIndebitati()) {
-			assertEquals(persona.getDebito(),new Euro(2));
-		}
-	}
-	
-	@Test
-	void testDiminuisciDebitoSize() {
-		for(Persona persona : personale.getPersonale()) {
-			persona.aumentaDebito(new Euro(5));
-			personale.diminuisciDebito(persona, new Euro(5));
-		}
-		assertEquals(personale.getIndebitati().size(),0);
-	}
-	
-	@Test
-	void testLoad() {
-		for(Persona persona : personale.getPersonale()) {
-			persona.aumentaDebito(new Euro(5));
-			personale.diminuisciDebito(persona,new Euro(3));
-		}
-		String str="";
-		try {
-			FileWriter fileWriter = new FileWriter("res/test.txt", false);
-			PrintWriter printWriter = new PrintWriter(fileWriter);
-			printWriter.print(str=personale.print());
-			printWriter.close();
-		} catch (IOException e) {
-		}
-		personale = new Personale();
-		personale.load("res/test.txt");
-		File file = new File("res/test.txt");
-        file.delete();
-		assertEquals(personale.print(),str);
-	}
-	
-	@Test
-	void testLoadFileNotExists() {
-		personale = new Personale();
-		personale.load("res/test.txt");
-		assertEquals(personale.getPersonale().size(),0);
-	}
-	
-	@Test
-	void testLoadFileWithNoRifornimenti() {
-		personale = new Personale();
-		File file = new File("res/test.txt");
-		personale.load("res/test.txt");
-        file.delete();
-		assertEquals(personale.getPersonale().size(),0);
-	}*/
-	
+
 }
