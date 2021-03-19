25a836f03f6a6e25b679fc78887c0b33873a7a6f
zanet <zanet@LAPTOP-HJOQC750>
2021-03-19 11:20:21 +0100

    Test cassa giusti

--- LaTazza/src/test/TestCassa.java
+++ LaTazza/src/test/TestCassa.java
@@ -20,7 +20,7 @@ import application.utils.Euro;
 public class TestCassa {
 
 	Cassa cassa=new Cassa();
-	Cassa cassaVuota=new Cassa();
+	Cassa cassaVuota=new Cassa();;
 	
 	@BeforeEach
 	void setUp() {
@@ -36,24 +36,24 @@ public class TestCassa {
 	void testEffettuaPagamentoSuperiore() {
 		int Disponibilità=(int)cassa.getDisponibilita().getValore();
 		assertFalse(cassa.effettuaPagamento(new Euro(Disponibilità+42)));
+		assertEquals(Disponibilità, cassa.getDisponibilita().getValore());
 	}
-
+	
+	
 	@Test
 	void testEffettuaPagamento() {
-		cassa.riceviPagamento(new Euro(100));
-		assertTrue(cassa.effettuaPagamento(new Euro(42)));
-		assertTrue(cassa.effettuaPagamento(new Euro(58)));
+		cassa.riceviPagamento(new Euro(0, 100));
+		assertTrue(cassa.effettuaPagamento(new Euro(0, 42)));
+		assertTrue(cassa.effettuaPagamento(new Euro(0, 58)));
 	}
 	
 	@ParameterizedTest
 	@CsvSource({ "42","10"})
 	void testRiceviPagamento(int ammontareInt) {
-		Euro disponibilitaIniziale = cassa.getDisponibilita();
-		Euro ammontareEuro = new Euro(ammontareInt); 
-		cassa.riceviPagamento(ammontareEuro);
-		assertTrue(cassa.getDisponibilita().ugualeA(disponibilitaIniziale.somma(ammontareEuro)));
-		cassa.effettuaPagamento(new Euro(42));
-		assertEquals(cassa.getDisponibilita(), disponibilitaIniziale);
+		Euro disponibilitaIniziale = new Euro (0 , cassa.getDisponibilita().getValore());
+		cassa.riceviPagamento(new Euro(0, ammontareInt));
+		assertTrue(cassa.getDisponibilita().ugualeA(disponibilitaIniziale.somma(new Euro(0, ammontareInt))));
+		cassa.effettuaPagamento(new Euro(0, ammontareInt));
 	}
 	
 	@ParameterizedTest
